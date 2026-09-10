09173fa4e5670049c963c79965e19d8735008edb
AdemolaAdedoyin <adedoyinademola397@gmail.com>
2026-09-10 15:46:00 -0500

    Register receipt storage service

--- apps/api/src/expenses/expenses.module.ts
+++ apps/api/src/expenses/expenses.module.ts
@@ -3,11 +3,12 @@ import { RolesGuard } from '../common/guards/roles.guard';
 import { PoliciesModule } from '../policies/policies.module';
 import { ExpensesController } from './expenses.controller';
 import { ExpensesService } from './expenses.service';
+import { ReceiptStorageService } from './receipt-storage.service';
 
 @Module({
   imports: [PoliciesModule],
   controllers: [ExpensesController],
-  providers: [ExpensesService, RolesGuard],
+  providers: [ExpensesService, ReceiptStorageService, RolesGuard],
   exports: [ExpensesService],
 })
 export class ExpensesModule {}
