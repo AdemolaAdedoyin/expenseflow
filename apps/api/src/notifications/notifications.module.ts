b60cd6541be0c0056ce1338cf54f29c4da493202
Adedoyin Ademola <adedoyinademola397@gmail.com>
2026-09-10 15:58:22 -0500

    refactor(notifications): register email provider adapters

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,7 +1,23 @@
-import { Global, Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
+import { Global, Module } from '@nestjs/common';
+import {
+  ConsoleEmailProvider,
+  EmailProviderService,
+  SesEmailProvider,
+} from './email-provider';
 import { NotificationsProcessor } from './notifications.processor';
 import { NotificationsService } from './notifications.service';
+
 @Global()
-@Module({ imports: [BullModule.registerQueue({ name: 'notifications' })], providers: [NotificationsService, NotificationsProcessor], exports: [NotificationsService] })
+@Module({
+  imports: [BullModule.registerQueue({ name: 'notifications' })],
+  providers: [
+    NotificationsService,
+    NotificationsProcessor,
+    ConsoleEmailProvider,
+    SesEmailProvider,
+    EmailProviderService,
+  ],
+  exports: [NotificationsService],
+})
 export class NotificationsModule {}
